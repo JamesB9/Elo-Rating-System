54e4cfb43c4a3e019208d53bad6a868d42be6cb7
J-B <57415099+JamesB9@users.noreply.github.com>
2021-03-31 22:11:55 +0100

    elo teams

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import random
 
-num_players = 100
-team_size = 2
-num_games = 10000
+num_players = 1000
+team_size = 1
+num_games = 100000
 k_factor = 32
+
+players = []
+queued = []
+players_unqueued = []
+full_teams = []
+
+ranks = ["Bronze", "Silver", "Gold", "Platinum", "Diamond"]
+ranked_players = [[], [], [], [], []]
+playing_elo_range = 200
+
+ranks_upper_bound = 1600
+ranks_size = ranks_upper_bound / (len(ranks) - 2)
 
 
 class Player:
@@ -13,20 +25,36 @@ class Player:
         self.std_dev = 10  # random.randint(1, 20)
         self.elo = 1000
         self.games_played = 0
+        self.rank = 0
+        ranked_players[0].append(self)
+        self.update_rank(0)
 
     def gen_game_score(self):
         self.games_played += 1
         return int(random.normalvariate(self.avg_skill, self.std_dev))
+
+    def update_rank(self, elo_change):
+        self.elo += elo_change
+        ranked_players[self.rank].remove(self)
+
+        if self.elo > ranks_upper_bound:
+            self.rank = 4
+        else:
+            for i in range(len(ranks)):
+                if self.elo < (i * ranks_size):
+                    self.rank = i
+                    break
+        ranked_players[self.rank].append(self)
 
     def __str__(self):
         return 'Player {self.name}, {self.elo}, {self.avg_skill}, {self.std_dev}, {self.games_played}'.format(self=self)
 
 
 class Team:
-    def __init__(self, name):
-        self.name = name
-        self.players = []
+    def __init__(self, *players):
+        self.players = list(players)
         self.elo = 0
+        self.calculate_avg_elo()
 
     def add_player(self, player):
         self.players.append(player)
@@ -38,13 +66,18 @@ class Team:
             elo += player.elo
         self.elo = elo / len(self.players)
 
+    def __iadd__(self, other):
+        self.players.extend(other.players)
+        self.calculate_avg_elo()
+
     def __contains__(self, item):
         return item in self.players
 
     def __str__(self):
-        s = "{}: ".format(self.name)
+        s = ""
         for player in self.players:
-            s += player.name + ", "
+            s += "{:>10} {:>4}, ".format(ranks[player.rank], str(player.elo))
+        s += str(self.elo)
         return s
 
     def __iter__(self):
@@ -58,6 +91,10 @@ class Game:
     def __init__(self, team_a, team_b):
         self.team_a = team_a
         self.team_b = team_b
+        #print("Game Found")
+        #print(team_a)
+        #print(team_b)
+        #print()
 
     def play(self):
         # Calculate Expected Scores
@@ -87,14 +124,12 @@ class Game:
         rating_change_b = k_factor * (score_b - expected_b)
 
         for player in self.team_a:
-            player.elo += int(rating_change_a)
+            player.update_rank(int(rating_change_a))
         for player in self.team_b:
-            player.elo += int(rating_change_b)
-
-        # print(expected_a, expected_b, rating_change_a, rating_change_b)
+            player.update_rank(int(rating_change_b))
 
 
-def print_players(players):
+def print_players():
     print(
         "{:>10} {:>15} {:>10} {:>10} {:>10} {:>10}".format("Rank", "Name", "Elo", "Avg Score", "Sigma", "Games Played"))
     for i, player in enumerate(players):
@@ -103,7 +138,8 @@ def print_players(players):
                                                                  player.games_played))
 
 
-def create_team(name, players, party=Team("Empty")):
+'''
+def create_team(name, party=Team("Empty")):
     team = Team(name)
     for player in party:
         team.add_player(player)
@@ -114,32 +150,88 @@ def create_team(name, players, party=Team("Empty")):
         team.add_player(player)
         players.remove(player)
     return team
+'''
 
 
-def match_make(players, party=Team("Empty")):
-    team_a = create_team("Team A", players, party)
-    team_b = create_team("Team B", players)
-    return team_a, team_b
+def queue(team):
+    queued.append(team)
 
 
-def play_games(games_count, players):
-    for i in range(games_count):
-        team_a, team_b = match_make(players.copy())
-        game = Game(team_a, team_b)
+def match_make(team):
+    if len(team) == team_size:
+        full_teams.append(team)
+    else:
+        merged_into = -1
+        for i, qteam in enumerate(queued):
+            if valid_team_merge(qteam, team):
+                qteam += team
+                merged_into = i
+                break
+
+        if merged_into == -1:
+            queued.append(team)
+        elif len(queued[merged_into]) == team_size:
+            full_teams.append(queued[merged_into])
+            queued.remove(queued[merged_into])
+
+
+def check_for_games():
+    # Match Full Teams
+    full_teams.sort(key=lambda t: t.elo, reverse=True)
+    full_team_a = None
+    full_team_b = None
+    for i, full_team in enumerate(full_teams):
+        if i + 1 < len(full_teams) and abs(full_team.elo - full_teams[i + 1].elo) < 100:
+            full_team_a = full_team
+            full_team_b = full_teams[i + 1]
+
+    if full_team_a is not None and full_team_b is not None:
+        game = Game(full_team_a, full_team_b)
         game.play()
+        full_teams.remove(full_team_a)
+        full_teams.remove(full_team_b)
+
+        for player in full_team_a:
+            players_unqueued.append(player)
+        for player in full_team_b:
+            players_unqueued.append(player)
+        return 1
+    return 0
+
+
+def valid_team_merge(team_a, team_b):
+    if len(team_a) + len(team_b) <= team_size:  # If team is within size
+        if abs(team_a.elo - team_b.elo) < playing_elo_range:  # If elo within range
+            return True
+    return False
 
 
 def main():
     # Create Players
-    players = []
     for i in range(num_players):
-        players.append(Player("Player {}".format(i + 1)))
+        # Create Player
+        player = Player("Player {}".format(i + 1))
+        players.append(player)
+        players_unqueued.append(player)
 
-    # print("PLAYERS BEFORE PLAYING {} GAMES".format(num_games))
-    # print_players(players)
+    for player in players_unqueued:
+        match_make(Team(player))
+        players_unqueued.remove(player)
 
-    play_games(num_games, players)
+    game_count = 0
+    while game_count < num_games:
+        games_played = check_for_games()
+        game_count += games_played
+        print("Games Played = ", game_count)
 
+        if random.random() < 0.1 or games_played == 0:
+            if games_played == 0 and len(players_unqueued) == 0:
+                print("FAILED")
+            for player in players_unqueued:
+                match_make(Team(player))
+                players_unqueued.remove(player)
+
+    '''
     shroud = Player("-=SHROUD=-")
     shroud.avg_skill = 90
     shroud.std_dev = 5
@@ -148,16 +240,13 @@ def main():
     party.add_player(shroud)
 
     for i in range(400):
-        team_a, team_b = match_make(players.copy(), party)
-        print(team_a.elo)
-        print(team_b.elo)
-        print()
+        team_a, team_b = match_make(party)
         game = Game(team_a, team_b)
         game.play()
-
+    '''
     players.sort(key=lambda p: p.elo, reverse=True)
     print("\nPLAYERS AFTER PLAYING {} GAMES".format(num_games))
-    print_players(players)
+    print_players()
 
 
 if __name__ == "__main__":
